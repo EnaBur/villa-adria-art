d9c225f5a09a6391b7564ea93d3a0afadab1eafb
EnaBur <ena.buric284@gmail.com>
2023-04-18 00:43:23 +0200

    images

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import data from '../data.js';
 
 const Gallery = () => {
 
+    const [image, setImage] = useState(data());
+
     return(
         <div className="galery-body" id="gallery">
             <div className="galery">
-                <img alt="img1" src={require('../images/1.jpg')}/>
-                <img alt="img1" src={require('../images/2.jpg')}/>
-                <img alt="img1" src={require('../images/3.jpg')}/>
-                <img alt="img1" src={require('../images/4.jpg')}/>
-                <img alt="img1" src={require('../images/5.jpg')}/>
-                <img alt="img1" src={require('../images/6.jpg')}/>
+            {image.map((i) => (
+            <img src={i.image} alt="house"
+                image = {image}
+                setImage = {setImage}
+                i = {i}
+            >
+            </img>
+            ))}
             </div>
         </div>
         
--- src/data.js
+++ src/data.js
@@ -3,26 +3,24 @@ import { v4 as uuidv4 } from "uuid";
 function imagesExport() {
   return [
         {
-            image: "",
+            image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/364779846.jpg?k=3cc72da685b162a5462cc52519b3c02495b834cfae986d32fb98d1a49f317684&o=&hp=1",
         },
         {
-            image: "",
+            image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/364779910.jpg?k=125b4bb3793efa4e18dc39d5ae040db77d42e7c9d57b3c84e8cb9fab674d8533&o=&hp=1",
         },
         {
-            image: "",
+            image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/364776404.jpg?k=733e87cb8b02787205d55eeba0f9f6fab2389b14204be95d24e766c4f866053f&o=&hp=1",
         },
         {
-            image: "",
+            image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/364772196.jpg?k=4af0b8c3cc7fc163cdfd5e5b565d73b42f4335c4f45509d7af59ccb4ef7d8855&o=&hp=1",
         },
         {
-            image: "",
+            image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/364772512.jpg?k=8420b03c172c665f8c307ed797e83bb7d01caf21a77822db18f55059c8afde0f&o=&hp=1",
         },
         {
-            image: "",
+            image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/364772722.jpg?k=c901b07faa2cd404c34cfe913b0e60f9146b58fb85ccf5731de69fd84009307c&o=&hp=1",
         },
-        {
-            image: "",
-        },
+        
     ];
 }
 
